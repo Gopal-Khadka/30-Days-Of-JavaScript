09327dde451bb299fe1bd53ebc3c48fe89442e20
Gopal Khadka <117714059+Gopal-Khadka@users.noreply.github.com>
2023-03-23 16:20:00 +0545

    Add files via upload

---
+++ Day 4.js
@@ -0,0 +1,42 @@
+// if else statements
+num = -3
+if (num > 0) {
+  console.log(`${num} is a positive number`)
+} else {
+  console.log(`${num} is a negative number`)
+}
+//  -3 is a negative number
+
+// else if
+let a = 0
+if (a > 0) {
+  console.log(`${a} is a positive number`)
+} else if (a < 0) {
+  console.log(`${a} is a negative number`)
+} else if (a == 0) {
+  console.log(`${a} is zero`)
+} else {
+  console.log(`${a} is not a number`)
+}
+
+// switch case
+let weather = 'cloudy'
+switch (weather) {
+  case 'rainy':
+    console.log('You need a rain coat.')
+    break
+  case 'cloudy':
+    console.log('It might be cold, you need a jacket.')
+    break
+  case 'sunny':
+    console.log('Go out freely.')
+    break
+  default:
+    console.log(' No need for rain coat.')
+}
+
+// ternary operators
+let isRaining = true
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
